e1a633edd1493e03754f087996d49bb1017f3887
Omprakash Sridharan <omprakashsridharan@outlook.com>
2022-08-07 11:31:38 +0200

    Adds gateway

---
+++ gateway/src/main.rs
@@ -0,0 +1,52 @@
+use axum::{
+    extract::Extension,
+    http::{uri::Uri, HeaderValue, Request, Response},
+    routing::get,
+    Router,
+};
+use hyper::{client::HttpConnector, Body};
+use std::net::SocketAddr;
+
+type Client = hyper::client::Client<HttpConnector, Body>;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+
+    let proxy = |service: String| {
+        return move |client, mut req: Request<Body>| {
+            req.headers_mut().append(
+                "service",
+                HeaderValue::from_str(service.clone().as_str()).unwrap(),
+            );
+            handler(client, req)
+        };
+    };
+
+    let app = Router::new()
+        .route("/user/*path", get(proxy(String::from("user"))))
+        .layer(Extension(client));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 4000));
+    println!("reverse proxy listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(Extension(client): Extension<Client>, mut req: Request<Body>) -> Response<Body> {
+    let path = req.uri().path();
+    let service_header = req.headers().get("service").unwrap();
+    let path_query = req
+        .uri()
+        .path_and_query()
+        .map(|v| v.as_str())
+        .unwrap_or(path);
+
+    let uri = format!("http://{}{}", service_header.to_str().unwrap(), path_query);
+    println!("Uri {:?}", uri);
+    *req.uri_mut() = Uri::try_from(uri).unwrap();
+
+    client.request(req).await.unwrap()
+}
